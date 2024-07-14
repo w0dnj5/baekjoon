c6f46e21be80c2b6dc922a0b6d006ef09fa2ec7b
wodnj5 <jw991211@icloud.com>
2024-07-15 08:24:56 +0900

    refactor: [28702] 수정

--- src/Baekjoon28702.java
+++ src/Baekjoon28702.java
@@ -4,31 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-enum FizzBuzz {
-
-    FIZZ("Fizz"),
-    BUZZ("Buzz"),
-    FIZZBUZZ("FizzBuzz");
-
-    private final String display;
-
-    FizzBuzz(String display) {
-        this.display = display;
-    }
-
-    public String getDisplay() {
-        return display;
-    }
-}
-
 public class Baekjoon28702 {
 
+    public static final String FIZZBUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
     public static final int INPUT_LENGTH = 3;
 
     // 3의 배수 && 5의 배수 = "FizzBuzz"
     // 3의 배수 && !5의 배수 = "Fizz"
     // !3의 배수 && 5의 배수 = "Buzz"
-    // !3배수 && !5의 배수 = i
+    // !3배수 && !5의 배수 = number
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,7 +26,7 @@ public class Baekjoon28702 {
         String result = null;
         for(int i = 0; i < INPUT_LENGTH; i++) {
             if(isInteger(inputs[i])) {
-                result = getResult(Integer.parseInt(inputs[i]) + (INPUT_LENGTH - i));
+                result = calculate(Integer.parseInt(inputs[i]) + (INPUT_LENGTH - i));
                 break;
             }
         }
@@ -51,16 +37,16 @@ public class Baekjoon28702 {
     public static boolean isInteger(String input) {
         try {
             Integer.parseInt(input);
+            return true;
         } catch (NumberFormatException e) {
             return false;
         }
-        return true;
     }
 
-    public static String getResult(int num) {
-        if(num % 3 != 0 && num % 5 == 0) return FizzBuzz.BUZZ.getDisplay();
-        if(num % 3 == 0 && num % 5 != 0) return FizzBuzz.FIZZ.getDisplay();
-        if(num % 3 == 0 && num % 5 == 0) return FizzBuzz.FIZZBUZZ.getDisplay();
+    public static String calculate(int num) {
+        if(num % 3 != 0 && num % 5 == 0) return BUZZ;
+        if(num % 3 == 0 && num % 5 != 0) return FIZZ;
+        if(num % 3 == 0 && num % 5 == 0) return FIZZBUZZ;
         return String.valueOf(num);
     }
 }
