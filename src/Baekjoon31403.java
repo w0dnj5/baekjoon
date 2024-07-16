d993fb22e4995ed4f0a46e2c825242d99bce803b
wodnj5 <jw991211@icloud.com>
2024-07-16 10:27:13 +0900

    feat: [31403 A + B - C]

---
+++ src/Baekjoon31403.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon31403 {
+
+    // A + B - C
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        System.out.println(A + B - C);
+        System.out.println(Integer.parseInt(A + "" + B) - C);
+    }
+}
